c4940632035d8baf8bc8517a8df5ea3b3d261072
dillon <dillonrhamilton@gmail.com>
2023-04-22 21:27:10 -0400

    add create

--- app/models/plant.server.ts
+++ app/models/plant.server.ts
@@ -1,4 +1,5 @@
 import { prisma } from "~/db.server";
+import type { Plant } from "@prisma/client";
 
 export async function getPlants() {
     return prisma.plant.findMany();
@@ -8,6 +9,8 @@ export async function getPlant(slug: string) {
     return prisma.plant.findUnique({ where: { slug } })
 }
 
-export async function createPlant(plant) {
+export async function createPlant(
+    plant: Pick<Plant, "slug" | "name" | "markdown">
+) {
     return prisma.plant.create({ data: plant })
 }
--- app/routes/plants/admin/new.tsx
+++ app/routes/plants/admin/new.tsx
@@ -1,24 +1,68 @@
-import { Form } from "@remix-run/react";
+import type { ActionArgs } from "@remix-run/node";
+import { redirect, json } from "@remix-run/node";
+import { Form, useActionData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+import { createPlant } from "~/models/plant.server";
+
+export const action = async ({ request }: ActionArgs) => {
+  const formData = await request.formData();
+
+  const name = formData.get("name");
+  const slug = formData.get("slug");
+  const markdown = formData.get("markdown");
+
+  const errors = {
+    name: name ? null : "Name is required",
+    slug: slug ? null : "Slug is required",
+    markdown: markdown ? null : "Markdown is required",
+  };
+  const hasErrors = Object.values(errors).some((errorMessage) => errorMessage);
+
+  if (hasErrors) {
+    return json(errors);
+  }
+
+  invariant(typeof name === "string", "name must be string");
+
+  invariant(typeof slug === "string", "slug must be string");
+
+  invariant(typeof markdown === "string", "markdown must be string");
+
+  await createPlant({ name, slug, markdown });
+
+  return redirect("/plants/admin");
+};
 
 const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
 
 export default function NewPlant() {
+  const errors = useActionData<typeof action>();
+
   return (
     <Form method="post">
       <p>
         <label>
-          Plant Title:{" "}
-          <input type="text" name="title" className={inputClassName} />
+          Plant Name:{" "}
+          {errors?.name ? (
+            <em className="text-red-600">{errors.name}</em>
+          ) : null}
+          <input type="text" name="name" className={inputClassName} />
         </label>
       </p>
       <p>
         <label>
           Plant Slug:{" "}
+          {errors?.slug ? (
+            <em className="text-red-600">{errors.slug}</em>
+          ) : null}
           <input type="text" name="slug" className={inputClassName} />
         </label>
       </p>
       <p>
-        <label htmlFor="markdown">Markdown:</label>
+        <label htmlFor="markdown">Markdown: </label>
+        {errors?.markdown ? (
+          <em className="text-red-600">{errors.markdown}</em>
+        ) : null}
         <br />
         <textarea
           id="markdown"
@@ -30,7 +74,7 @@ export default function NewPlant() {
       <p className="text-right">
         <button
           type="submit"
-          className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+          className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
         >
           Create Plant
         </button>
